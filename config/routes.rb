27df15ae335d66bb2595a701b52a7c060867e51f
Jason Hertzbach <hertzbachj@gmail.com>
2017-08-08 12:23:16 -0400

    added navbar/ changed routes/ added jquery with yarn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :cities
   root 'sessions#new'
-  resource :users
-  resource :sessions
+  resources :users
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
